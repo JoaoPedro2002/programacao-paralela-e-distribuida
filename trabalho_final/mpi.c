161f53f93cbbd94c63ae120253979fce6dd533b4
João Pedro Cardoso Barbosa <joao.c.barbosa2002@gmail.com>
2022-07-23 11:19:38 -0300

    MPI working

--- trabalho_final/mpi.c
+++ trabalho_final/mpi.c
@@ -6,14 +6,12 @@
 #include <mpi.h>
 #include "aes.h"
 
-uint8_t *buf;
-
-
 typedef struct MPI_attrs {
 uint8_t* key;
 uint8_t vector[AES_BLOCKLEN];
 size_t start_position;
 size_t size_slice;
+uint8_t* buffer;
 } MPI_attrs;
 
 void create_buffer(uint8_t *buffer, unsigned long tam, unsigned padding){
@@ -45,7 +43,7 @@ void mpi_xcrypt(MPI_attrs *attrs) {
   for (size_t i = 0; i < attrs->size_slice; i+=AES_BLOCKLEN) {
     AES_cipher((state_t *)attrs->vector, attrs->key);
     for(int j = 0; j < AES_BLOCKLEN; j++) {
-      buf[attrs->start_position + i + j] = buf[attrs->start_position + i + j] ^ attrs->vector[j];
+      attrs->buffer[i + j] = attrs->buffer[i + j] ^ attrs->vector[j];
     }
     increment_buffer(attrs->vector, 1);
   }
@@ -54,6 +52,7 @@ void mpi_xcrypt(MPI_attrs *attrs) {
 int main(void) {
   srand((unsigned)time(NULL));
 
+  uint8_t *buf;
   size_t size;
   size_t len = 64;
   unsigned padding = 0;
@@ -82,41 +81,49 @@ int main(void) {
   MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
   int world_size;
   MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+  #if defined(VERBOSE) && (VERBOSE==1)
   printf("%d\n",world_rank);
+  #endif
   size_t slice = size / world_size;
 
   if (world_rank == 0) {
     printf("----MPI----\n");
 
     #if defined(VERBOSE) && (VERBOSE == 1)
-    printf("plain: ");
+    printf("plain:     ");
     phex(buf, size);
     printf("\n");
     #endif
+    gettimeofday(&t1, NULL);
   }
 
-  //uint8_t *buffer_slice = (uint8_t *)malloc(sizeof(uint8_t) * size / N_PROCESS);
+  uint8_t *buffer_slice = (uint8_t *)malloc(sizeof(uint8_t) * size / world_size);
 
-  //MPI_Scatter(buf, slice, MPI_UINT8_T, buffer_slice, 
-  //slice, MPI_UINT8_T, 0, MPI_COMM_WORLD);
-
+  MPI_Scatter(buf, slice, MPI_UINT8_T, buffer_slice, 
+  slice, MPI_UINT8_T, 0, MPI_COMM_WORLD);
   MPI_attrs attrs;
   attrs.key = ctx.RoundKey;
   memcpy(attrs.vector, ctx.Iv, AES_BLOCKLEN);
   increment_buffer(attrs.vector, (slice / AES_BLOCKLEN) * world_rank);
-  attrs.start_position = slice * world_rank;
   attrs.size_slice = slice;
+  attrs.buffer = buffer_slice;
   mpi_xcrypt(&attrs);
 
+  MPI_Gather(buffer_slice, slice, MPI_UINT8_T, 
+  buf, slice, MPI_UINT8_T, 0, MPI_COMM_WORLD);
 
-  //free(buffer_slice);
+  free(buffer_slice);
   MPI_Barrier(MPI_COMM_WORLD);
   if (world_rank == 0) {
     #if defined(VERBOSE) && (VERBOSE == 1)
     printf("encrypted: ");
     phex(buf, size);
     printf("\n");
-    #endif
+    #endif 
+    gettimeofday(&t2, NULL);
+    time = (t2.tv_sec - t1.tv_sec) + ((t2.tv_usec - t1.tv_usec)/1000000.0);
+	  printf("tempo para encriptar = %f\n", time);
+  
   }
 
   if (world_rank == 0) {
